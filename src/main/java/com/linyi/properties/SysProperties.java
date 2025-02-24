83348673a148102e12ca63850d1953bac2d3b3a6
镜花水月 <14131958+hsdchb@user.noreply.gitee.com>
2025-02-24 21:58:42 +0800

    初始化

---
+++ src/main/java/com/linyi/properties/SysProperties.java
@@ -0,0 +1,24 @@
+package com.linyi.properties;
+
+
+import com.linyi.generator.entity.GeneratorConfig;
+import lombok.Data;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @Author: linyi
+ * @Date: 2025/2/24
+ * @ClassName: SysProperties
+ * @Version: 1.0
+ * @Description: 读取系统配置文件
+ */
+@SpringBootConfiguration
+@PropertySource(value = {"classpath:system.properties"})// 注意,只能从.properts文件取值,yml不行
+@ConfigurationProperties(prefix = "sys") //从全局配置文件取值
+@Data
+public class SysProperties {
+    private Integer aa;
+    private GeneratorConfig generator;
+}
